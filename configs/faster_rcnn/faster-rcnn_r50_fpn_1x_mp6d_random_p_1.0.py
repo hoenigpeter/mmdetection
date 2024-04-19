b642b1689c58eb39306568382ee0cb3c0c3b2af0
Peter Hoenig <hpfreak1004@gmail.com>
2024-04-19 21:48:44 +0200

    more confiiiigs

---
+++ configs/faster_rcnn/faster-rcnn_r50_fpn_1x_mp6d_random_p_1.0.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/faster-rcnn_r50_fpn_mp6d.py',
+    '../_base_/datasets/mp6d_random_detection_p_1.0.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
