438e71f6bcb416fe281790ea8fa57869a8f5e816
Peter Hoenig <hpfreak1004@gmail.com>
2024-04-26 20:04:01 +0200

    bugfix

--- configs/yolox/yolox_s_8xb8-300e_mp6d_random_p_0.8_s_0.34.py
+++ configs/yolox/yolox_s_8xb8-300e_mp6d_random_p_0.8_s_0.34.py
@@ -102,7 +102,7 @@ train_pipeline = [
         ratio_range=(0.8, 1.6),
         pad_val=114.0),
     #dict(type='YOLOXHSVRandomAug'),
-    dict(type='RandomRGB', p=0.8, scaling_factor=1/3),
+    dict(type='RandomRGB', p=0.8, scaling_factor=1/3, augmentation_indices=[]),
     dict(type='RandomFlip', prob=0.5),
     # According to the official implementation, multi-scale
     # training is not considered here but in the
