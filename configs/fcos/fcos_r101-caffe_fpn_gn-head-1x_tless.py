9734d37abd4f9a4ca126a9ad3e473b4bce9c7a64
Peter Hoenig <hpfreak1004@gmail.com>
2024-05-06 08:17:10 +0200

    update

---
+++ configs/fcos/fcos_r101-caffe_fpn_gn-head-1x_tless.py
@@ -0,0 +1,9 @@
+_base_ = './fcos_r50-caffe_fpn_gn-head_1x_tless_p_1.0.py'
+
+# model settings
+model = dict(
+    backbone=dict(
+        depth=101,
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint='open-mmlab://detectron/resnet101_caffe')))
