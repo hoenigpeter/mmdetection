1006840aec5d205e7530873c5372224d3a7cfce0
Peter Hoenig <hpfreak1004@gmail.com>
2024-06-01 15:25:09 +0200

    update

---
+++ configs/mask_rcnn/mask-rcnn_r50_fpn_2x_tless.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/mask-rcnn_r50_fpn_tless.py',
+    '../_base_/datasets/tless_instance.py',
+    '../_base_/schedules/schedule_30.py', '../_base_/default_runtime.py'
+]
