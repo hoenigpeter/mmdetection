cfb44a48788059a06cd1003f4863ca7d6d72f691
Peter Hoenig <hpfreak1004@gmail.com>
2024-06-01 14:50:53 +0200

    bugfix§

--- configs/mask_rcnn/mask-rcnn_r50_fpn_2x_tless_random_texture_p_0.py
+++ configs/mask_rcnn/mask-rcnn_r50_fpn_2x_tless_random_texture_p_0.py
@@ -1,5 +1,5 @@
 _base_ = [
     '../_base_/models/mask-rcnn_r50_fpn_tless.py',
-    '../_base_/datasets/tless_random_detection_p_0.py',
+    '../_base_/datasets/tless_random_instance_p_0.py',
     '../_base_/schedules/schedule_30.py', '../_base_/default_runtime.py'
 ]
