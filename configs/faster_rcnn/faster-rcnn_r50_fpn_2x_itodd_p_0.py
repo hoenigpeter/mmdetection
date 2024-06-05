525f3fcd22a13efeddb0f08d2de5a649a10e20fa
Peter Hoenig <hpfreak1004@gmail.com>
2024-06-05 15:17:31 +0200

    added itodd

---
+++ configs/faster_rcnn/faster-rcnn_r50_fpn_2x_itodd_p_0.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/faster-rcnn_r50_fpn_itodd.py',
+    '../_base_/datasets/itodd_detection_p_0.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
