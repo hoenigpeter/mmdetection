3447c864b5019ab7be7715945ad2ab01d81d8dcc
Peter Hoenig <hpfreak1004@gmail.com>
2024-07-01 11:05:12 +0200

    moooooore confiiiiigs

--- configs/faster_rcnn/faster-rcnn_r50_fpn_2x_tless_p_0.py
+++
@@ -1,5 +0,0 @@
-_base_ = [
-    '../_base_/models/faster-rcnn_r50_fpn.py',
-    '../_base_/datasets/tless_detection_p_0.py',
-    '../_base_/schedules/schedule_30.py', '../_base_/default_runtime.py'
-]
