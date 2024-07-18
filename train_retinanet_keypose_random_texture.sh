f6a7393d4470b430f0c010bbfdff64b26e787a5f
Peter Hoenig <hpfreak1004@gmail.com>
2024-07-18 16:42:04 +0200

    update

---
+++ train_retinanet_keypose_random_texture.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=retinanet_keypose_random_texture
+#SBATCH --partition=GPU-v100
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=4
+#SBATCH --time=120:00:00
+#SBATCH --output=slurm_retinanet_keypose_random_texture
+#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --mail-user=hoenig@acin.tuwien.ac.at
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+python tools/train.py configs/retinanet/retinanet_r50_fpn_2x_keypose_random_texture.py --auto-scale-lr
